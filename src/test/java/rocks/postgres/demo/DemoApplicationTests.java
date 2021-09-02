b3c475b7c9f5261c2c46bbee016cd1d1528e356b
Dave Cramer <davecramer@gmail.com>
2021-09-02 14:00:37 -0400

    initial commit

---
+++ src/test/java/rocks/postgres/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.postgres.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
